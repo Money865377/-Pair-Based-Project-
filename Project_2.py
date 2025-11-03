f28eec1b51010b0913ad476cfe6f72aca9542660
hfall123 <hfall009@uottawa.ca>
2025-11-03 16:07:34 -0500

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -26,69 +26,77 @@ print("""
 This program will measure an compare the expected and actual PWM signals of 2 Picos.
 To indicate things are going well, LED 1 will turn on.
 To indicate an error, LED 2 will turn on.      
-
-If you would like to procede, press the sw5 button.
       """)
 
+#set the duty cycle to whatever the user would like it to be (between 0 and 65536)
+duty_cycle = input("If you would like to procede, give me a duity cycle (any number between 0 and 65536): ")
+
+#make sure the number that was given was between 0 and 65536
+if duty_cycle >= 0 and duity_cycle <= 65536:
+    #if it is, continue
+    print("press the sw5button to continue")
+else:
+    #if it is not, then notify the user and ask them to try again
+    print("I said a number between 0 and 65536, try again")
+
 #while the statemetn is true, run this loop
-while True:
+while True: 
     if sw5.value():
-        #send a message to the reciving pico
-        for duty in range(0, 65536, 8192):  # Step through duty cycles
-            pwm.duty_u16(duty)              # Set PWM duty cycle
-            time.sleep(0.5)                 # Allow filter to settle
+        #send a message to the reciving pico 
+        pwm.duty_u16(duty_cycle)        # Set PWM duty cycle
+        time.sleep(0.5)                 # Allow filter to settle
 
-            #find out weather the signal has been recieved sucsessfully
+        #find out weather the signal has been recieved sucsessfully
+        try:
+            #Read analog voltage
+            adc_value = adc.read_u16()
+            print("message recieved")
+            #if the message is recieved, blink the LED
+            led1.value(1)
+        except: 
+            #if the message is not recieved, notify user
+            print("Error: there is no signal being read")
+            led1.value(0)
+        
+        #find the duty cycle percentage
+        duty_percent = (duty / 65535) * 100
+
+        #print the results to the user
+        message = f"{duty_percent:.1f},{adc_value}\n"
+        uart.write(message)
+
+        #sleep for 0.5 seconds to give the devices time to send and recieve signals smoothly
+        time.sleep(0.5)   
+
+    pwm.deinit()
+
+    #if there is a response, read it, if not, notify user
+    if uart.any(): 
+        #Read one line of text
+        data = uart.readline() 
+        if data:
+            #Blink LED to show message received
+            led1.toggle()       
             try:
-                #Read analog voltage
-                adc_value = adc.read_u16()
-                print("message recieved")
-                #if the message is recieved, blink the LED
-                led1.value(1)
-            except: 
-                #if the message is not recieved, notify user
-                print("Error: there is no signal being read")
-                led1.value(0)
-            
-            #find the duty cycle percentage
-            duty_percent = (duty / 65535) * 100
-
-            #print the results to the user
-            message = f"{duty_percent:.1f},{adc_value}\n"
-            uart.write(message)
-
-            #sleep for 0.5 seconds to give the devices time to send and recieve signals smoothly
-            time.sleep(0.5)   
-
-        pwm.deinit()
-
-        #if there is a response, read it, if not, notify user
-        if uart.any(): 
-            #Read one line of text
-            data = uart.readline() 
-            if data:
-                #Blink LED to show message received
-                led1.toggle()       
-                try:
-                    #decode the recieved information to ensure the user can read and understand it
-                    duty_str, adc_str = data.decode().strip().split(',')
-                    #change the type of the infromation from the duty cycle to float
-                    duty = float(duty_str)
-                    #change the type of the infromation from the duty cycle to integer
-                    adc = int(adc_str)
-                    #print the recieved duty cycle
-                    print(f"Received -> Duty Cycle: {duty:.1f}% | ADC: {adc}")
-                except:
-                    #notify user if there is an issue decoding the signal from the pico
-                    print("Error parsing:", data)
-            else:
-                #Blink LED and notify the users that the signal has not been recieved
-                print("Error: there is no signal being read")
-                led2.toggle()
-            #allow the picos 0.2 seconds to recive data so that they do not get overwhelmed
-            time.sleep(0.2)
+                #decode the recieved information to ensure the user can read and understand it
+                duty_str, adc_str = data.decode().strip().split(',')
+                #change the type of the infromation from the duty cycle to float
+                duty = float(duty_str)
+                #change the type of the infromation from the duty cycle to integer
+                adc = int(adc_str)
+                #print the recieved duty cycle
+                print(f"Received -> Duty Cycle: {duty:.1f}% | PWM: {adc}")
+            except:
+                #notify user if there is an issue decoding the signal from the pico
+                print("Error parsing:", data)
         else:
-            #notify user if there is no reply to read
+            #Blink LED and notify the users that the signal has not been recieved
+            print("Error: there is no signal being read")
+            led2.toggle()
+        #allow the picos 0.2 seconds to recive data so that they do not get overwhelmed
+        time.sleep(0.2)
+    else:
+        #notify user if there is no reply to read
+        print("Error: there has been an issue reading the ADC signal")
 
-            print("Error: there has been an issue reading the ADC signal")
 
